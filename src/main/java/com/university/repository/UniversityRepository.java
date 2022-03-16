46ceb583f527e0de46f1baeab295edadf5721f7a
sujitjava1988 <101707499+sujitjava1988@users.noreply.github.com>
2022-03-16 16:47:39 +0530

    Add files via upload

--- src/main/java/com/university/repository/UniversityRepository.java
+++ src/main/java/com/university/repository/UniversityRepository.java
@@ -1,1 +1,9 @@
+package com.university.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.university.entity.University;
+
+public interface UniversityRepository extends JpaRepository<University, Long>{
+
+}
