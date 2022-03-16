cb7db97d304edea4eb96cb47dac29ea22f9bf9ff
sujitjava1988 <101707499+sujitjava1988@users.noreply.github.com>
2022-03-16 16:47:07 +0530

    Add files via upload

--- src/main/java/com/university/entity/University.java
+++ src/main/java/com/university/entity/University.java
@@ -1,1 +1,145 @@
+package com.university.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "university")
+public class University {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+
+	private String email;
+
+	private String country_dial_code;
+
+	private String mobile;
+
+	private String state;
+
+	private String city;
+
+	private String campus;
+
+	private String source;
+
+	private String medium;
+
+	private String campaign;
+
+	private String secret_key;
+
+	private String college_id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCountry_dial_code() {
+		return country_dial_code;
+	}
+
+	public void setCountry_dial_code(String country_dial_code) {
+		this.country_dial_code = country_dial_code;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCampus() {
+		return campus;
+	}
+
+	public void setCampus(String campus) {
+		this.campus = campus;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getMedium() {
+		return medium;
+	}
+
+	public void setMedium(String medium) {
+		this.medium = medium;
+	}
+
+	public String getCampaign() {
+		return campaign;
+	}
+
+	public void setCampaign(String campaign) {
+		this.campaign = campaign;
+	}
+
+	public String getSecret_key() {
+		return secret_key;
+	}
+
+	public void setSecret_key(String secret_key) {
+		this.secret_key = secret_key;
+	}
+
+	public String getCollege_id() {
+		return college_id;
+	}
+
+	public void setCollege_id(String college_id) {
+		this.college_id = college_id;
+	}
+
+}
