10594f12428bed935c821b461bf25bc8371e19c3
sujitjava1988 <101707499+sujitjava1988@users.noreply.github.com>
2022-03-16 16:48:41 +0530

    Add files via upload

--- src/main/java/com/university/utility/UniversityValidation.java
+++ src/main/java/com/university/utility/UniversityValidation.java
@@ -1,1 +1,32 @@
+package com.university.utility;
 
+import com.university.entity.University;
+
+public class UniversityValidation {
+
+	public static boolean validate(University university) {
+		boolean isValid = true;
+		if (university != null) {
+			String collegeId = university.getCollege_id();
+			String email = university.getEmail();
+			String countryDialCode = university.getCountry_dial_code();
+			String mobileNumber = university.getMobile();
+			String source = university.getSource();
+			String secretKey = university.getSecret_key();
+			if (isBlank(collegeId) || isBlank(email) || isBlank(countryDialCode) || isBlank(mobileNumber)
+					|| isBlank(source) || isBlank(secretKey)) {
+				isValid = false;
+			}
+		}
+		return isValid;
+	}
+
+	private static boolean isBlank(String value) {
+		if (value == null || (value != null && value.isEmpty())) {
+			return true;
+		}
+
+		return false;
+	}
+
+}
